23c8f4e1cef582c7222e160d6110fa29d9523419
Maryna Haiduk <maryna.i.haiduk@gmail.com>
2024-10-29 12:45:08 -0400

    Created CSharpTheoryProject with Method class inside.

---
+++ HappyArraySolutions/CSharpTheoryProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharpTheoryProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
