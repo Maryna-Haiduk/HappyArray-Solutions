d44aaaf96a548c2395bc2b70ebbd3239e582699a
Maryna Haiduk <maryna.i.haiduk@gmail.com>
2024-10-29 14:01:42 -0400

    Added Method CountWordsWithMoreThanFourLetters

--- HappyArraySolutions/CSharpTheoryProject/Methods.cs
+++ HappyArraySolutions/CSharpTheoryProject/Methods.cs
@@ -8,6 +8,21 @@ namespace CSharpTheoryProject
 {
     internal class Methods
     {
-        
+        public static int CountWordsWithMoreThanFourLetters(string str) 
+        { 
+            int count = 0;
+
+            string[] words = str.Split(' ').ToArray();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length >= 4)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
     }
 }
--- HappyArraySolutions/CSharpTheoryProject/Program.cs
+++ HappyArraySolutions/CSharpTheoryProject/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace CSharpTheoryProject
+﻿using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTheoryProject
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Create your string to count the number of words in it that have more than 4 letters: ");
+            string moreThanFourLetters = Console.ReadLine();
+            Console.WriteLine("Number of words with more then 4 letters: " + Methods.CountWordsWithMoreThanFourLetters(moreThanFourLetters));
         }
     }
 }
